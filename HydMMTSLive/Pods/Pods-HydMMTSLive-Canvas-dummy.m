d891cfa0ff1f4fadc86294595effd21e35fb3f57
Jagadeesh <connect_tp15@imobigeeks.com>
2014-07-25 21:19:25 +0530

    Updated the Pod file and created the Structure for core data

---
+++ HydMMTSLive/Pods/Pods-HydMMTSLive-Canvas-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HydMMTSLive_Canvas : NSObject
+@end
+@implementation PodsDummy_Pods_HydMMTSLive_Canvas
+@end
---
+++ HydMMTSLive/Pods/Pods-HydMMTSLive-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HydMMTSLive : NSObject
+@end
+@implementation PodsDummy_Pods_HydMMTSLive
+@end
